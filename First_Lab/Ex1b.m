b7ce48de64f80bc9e0c005e8478a850dd4f8ef35
Rajkumar Conjeevaram Mohan <Rajkumar.Conjeevaram-Mohan14@imperial.ac.uk>
2015-10-25 10:04:05 +0000

    Enabled large step size, and deleted unncessary commented code

--- First_Lab/Ex1b.m
+++ First_Lab/Ex1b.m
@@ -1,12 +1,12 @@
 function Ex1b
 
-%dt1 = 0.5; % large step size
+dt1 = 0.1; % large step size
 dt2 = 0.01; % smaller step size
 % Time points
 Tmin = 0;
 Tmax = 100;
 
-%T2 = Tmin:dt1:Tmax;
+T2 = Tmin:dt1:Tmax;
 T3 = Tmin:dt2:Tmax;
 
 m = 1;
@@ -14,28 +14,33 @@ c = 0.1;
 k = 1;
 
 % % Euler method with large step size
-% y2(1) = 1;
-% for t = 2:length(T2)
-%    y2(t) = -(1/m)*(c*(0+(k*y2(t-1))));
-%    y2(t) = y2(t-1)+(dt1*y2(t));
-% end
+
+v(1) = 1;
+v2(1) = 0;
+
+ for t = 1:length(T2)-1
+    v(t+1) = v(t)+dt1*v2(t);
+    f = -(1/m)*((c*v2(t))+(k*v(t)));
+    v2(t+1) = v2(t)+(dt1*f);
+ end
+
+figure
+plot(T2,v,'Color','Blue')
+ylim([-1 1]);
+ 
 % Euler method with smaller step size
 
 z(1) = 1;
 w(1) = 0;
 
 for t = 1:length(T3)-1
-    
-   %w( t+1 ) = w(t) + ( dt2 * t * ( -w(t) + sin( t * z(t) ) ) );
-   %z( t+1 ) = z(t) + ( dt2 * t * w(t) );
-   
+
    z ( t+1 ) = z(t) + ( dt2 * w(t) );
    w ( t+1 ) = w(t) + ( dt2 * ( -1/m * ( ( c * w(t) ) + ( k * z(t) ) ) ) ); 
-   
-   %y3(t) = (-1/m)*((c*0)+(k*y3(t-1)));
-   %y3(t) = y3(t-1)+(dt2*y3(t));
+
 end
 
+figure
 plot(T3,z,'Color','Red')
 ylim([-1 1]);
 
